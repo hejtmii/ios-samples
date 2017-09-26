8028a212b062ced6f31d2cb47e42380f4d4e498f
martinh <iva@iva-macbook-pro.local>
2017-09-26 21:09:57 +0200

    Initial content

--- Samples/ViewController.swift
+++ Samples/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var content: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let margins = content.layoutMarginsGuide
+        let count = 100
+        var lastLabel = UILabel.init()
+        content.translatesAutoresizingMaskIntoConstraints = false
+        
+        for i in 0 ..< count {
+            let label = UILabel.init()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            
+            content.addSubview(label)
+            
+            label.text = "Label \(i)"
+            label.font = label.font.withSize(CGFloat(1.5 * Double(i)))
+            
+            if (i == 0) {
+                label.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: lastLabel.layoutMarginsGuide.bottomAnchor).isActive = true
+            }
+            label.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+            if (i == count - 1) {
+                label.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+            }
+            
+            lastLabel = label
+        }
     }
 
     override func didReceiveMemoryWarning() {
